afbd5d522103f9466bb669753243e7d0e3a94a0c
rya-hdra <jiafpo278@gmail.com>
2020-09-27 23:59:47 +0900

    Angularフロントエンド側作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductModule } from './product/product.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  // { path: 'detail', component: ProductDetailComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), ProductModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+// productファイルなど、用途ごとのモジュールを読み込むファイル
+// router-outletの中身を記述するタイプスクリプトファイル
+// シングルページアプリケーションを作る際は、router-outletを経由させ、URLごとに表示を切り替える設定をする
